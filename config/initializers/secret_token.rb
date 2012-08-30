f25c6329e893d348da26c45accc9a425bb0fede9
Georgios S. Kampouris <alsnapone@hotmail.gr>
2012-08-30 21:27:38 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parlaa::Application.config.secret_token = '306358683b1fdba92832835bacc03094a26bf8d5fb80bb01106e337a7d4864e3e0c352188b60e64114c5fb18e8dc101d19265fac4e62cff1a981f8b12cd24e41'
